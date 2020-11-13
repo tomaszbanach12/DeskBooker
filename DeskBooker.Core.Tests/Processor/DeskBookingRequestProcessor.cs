76adeef1e189eeb4de82f74e7f5c9cddc91c9fb2
Tomasz B <tomaszbanach12@gmail.com>
2020-11-13 15:16:24 +0100

    Turn [ShouldReturnDeskBookingResultWithRequestValues] green

---
+++ DeskBooker.Core.Tests/Processor/DeskBookingRequestProcessor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DeskBooker.Core.Processor
+{
+    internal class DeskBookingRequestProcessor
+    {
+        public DeskBookingRequestProcessor()
+        {
+        }
+
+        internal DeskBookingResult BookDesk(DeskBookingRequest request)
+        {
+            return new DeskBookingResult
+            {
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Email = request.Email,
+                Date = request.Date
+            };
+        }
+    }
+}
---
+++ DeskBooker.Core.Tests/Processor/DeskBookingResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DeskBooker.Core.Processor
+{
+    internal class DeskBookingResult
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
